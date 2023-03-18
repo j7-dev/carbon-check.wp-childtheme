9841c7a527a26e3587693f62f406cdd457003ca1
J7 <j7.dev.gg@outlook.com>
2023-03-18 18:44:17 +0800

    feat: translate email

---
+++ ultimate-member/email/welcome_email.php
@@ -0,0 +1,27 @@
+<div style="max-width: 560px;padding: 20px;background: #ffffff;border-radius: 5px;margin:40px auto;font-family: Open Sans,Helvetica,Arial;font-size: 15px;color: #666">
+
+	<div style="color: #444444;font-weight: normal">
+		<div style="text-align: center;font-weight:600;font-size:26px;padding: 10px 0;border-bottom: solid 3px #eeeeee">{site_name}</div>
+		
+		<div style="clear:both"></div>
+	</div>
+	
+	<div style="padding: 0 30px 30px 30px;border-bottom: 3px solid #eeeeee">
+
+		<div style="padding: 30px 0;font-size: 24px;text-align: center;line-height: 40px">感謝您的註冊！<span>您的帳號已經啟用</span></div>
+
+		<div style="padding: 10px 0 50px 0;text-align: center"><a href="{login_url}" style="background: #555555;color: #fff;padding: 12px 30px;text-decoration: none;border-radius: 3px;letter-spacing: 0.3px">登入</a></div>
+		
+		<div style="padding:20px">如果您有任何問題，請聯絡我們 <a href="mailto:{admin_email}" style="color: #3ba1da;text-decoration: none">{admin_email}</a></div>
+		
+		
+	</div>
+	
+	<div style="color: #999;padding: 20px 30px">
+
+		<div>誠摯地感謝！</div>
+		<div><a href="{site_url}" style="color: #3ba1da;text-decoration: none">{site_name}</a>團隊</div>
+		
+	</div>
+
+</div>
