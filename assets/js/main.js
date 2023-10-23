76b6350e97c87b05954aea91e18f83ce093b7dc9
J7 <j7.dev.gg@outlook.com>
2023-10-24 01:54:41 +0800

    feat: 新增多語系對應字串

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,5 +14,52 @@
       const diff = windowH - pageH - headerH - footerH;
       $(".min-h-100vh").css("min-height", pageH + diff);
     }
+
+    translate();
   });
+
+  function translate() {
+    const mapping = {
+      "Log out": "登出",
+      "Account or E-mail": "帳號或 E-mail",
+      Password: "密碼",
+      Register: "註冊",
+      Account: "帳號",
+      Name: "姓名",
+      "E-mail": "電子郵件地址",
+      "Confirm Password": "確認Password",
+      "Log in": "登入",
+    };
+
+    // 遍歷mapping物件，並在selector內查找包含特定文本的元素，然後替換文本
+    $.each(mapping, function (key, value) {
+      $(`a:contains("${value}"), label:contains("${value}")`).each(function () {
+        $(this).html($(this).html().replace(value, key));
+      });
+    });
+
+    const formMapping = {
+      Login: "登入",
+      Register: "註冊",
+    };
+
+    $.each(formMapping, function (key, value) {
+      $(`input[value="${value}"]`).each(function () {
+        $(this).val(key);
+      });
+    });
+
+    const placeholderMapping = {
+      "Only English and numbers are allowed": "只能輸入英文、數字",
+      "Password must contain at least one lowercase letter, one uppercase letter, and one number":
+        "您的密碼必須至少包含一個小寫字母，一個大寫字母和一個數字",
+      "Confirm Password": "確認密碼",
+    };
+
+    $.each(placeholderMapping, function (key, value) {
+      $(`input[placeholder="${value}"]`).each(function () {
+        $(this).attr("placeholder", key);
+      });
+    });
+  }
 })(jQuery);
